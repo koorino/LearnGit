06a8f511d5ba9a72128e604df8900310bce2a600
koorino <arch_mager@126.com>
2015-01-29 23:30:28 +0800

    this is a Git test

---
+++ learnGit/learnGit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  learnGit
+//
+//  Created by Junfeng on 1/29/15.
+//  Copyright (c) 2015 ACFunclub. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ learnGit/learnGit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  learnGit
+//
+//  Created by Junfeng on 1/29/15.
+//  Copyright (c) 2015 ACFunclub. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
